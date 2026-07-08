6c54f94a37aafff1c453e075ddbd0841fc9309a7
osp2ow <osp2ow@gmail.com>
2026-07-08 23:34:15 +0900

    Delete src directory

--- src/modes/ja/deckConfig.js
+++
@@ -1,8 +0,0 @@
-// 덱 종류별로 "정답" 텍스트를 어느 필드에서 가져올지 정의
-
-export const ANSWER_GETTERS = {
-  words: (item) => item.meaning,
-  grammar: (item) => item.title,
-  kanji: (item) => item.meaning,
-  travel: (item) => item.korean,
-};
--- src/modes/kr/deckConfig.js
+++
@@ -1,8 +0,0 @@
-// 덱 종류별로 "정답(일본어 뜻)" 텍스트를 어느 필드에서 가져올지 정의
-// 이 앱은 일본인이 한국어를 배우는 앱이므로, 문제는 한국어로 나오고 보기는 일본어 뜻입니다.
-
-export const ANSWER_GETTERS = {
-  words: (item) => item.meaningJp,
-  grammar: (item) => item.titleJp,
-  travel: (item) => item.japanese,
-};
