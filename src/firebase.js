273d8b7f304c98959eb78bad8b5aecd3ccec7f41
osp2ow <osp2ow@gmail.com>
2026-07-08 17:03:59 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -16,14 +16,13 @@ import {
   serverTimestamp,
 } from 'firebase/firestore';
 
-// ⚠️ 이 부분만 본인 Firebase 프로젝트 값으로 교체하세요
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyCc__tTddKVmbMEvVxVBK0EdiRsLIoljGQ",
+  authDomain: "jp-study-5d3ef.firebaseapp.com",
+  projectId: "jp-study-5d3ef",
+  storageBucket: "jp-study-5d3ef.firebasestorage.app",
+  messagingSenderId: "187509468168",
+  appId: "1:187509468168:web:133cc49a5b9f60ebbcf5bd"
 };
 
 const app = initializeApp(firebaseConfig);
